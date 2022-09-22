e88fb1b941b6172a3cd66583163ce82b277787ca
Arthur.Svensson <guimaraesarthur86@yahoo.com.br>
2022-09-22 09:54:00 -0300

    Criação do projeto com Gradle, TestNG e primeiro teste com REST-assured.

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,36 @@
+package petstore;
+
+import org.testng.annotations.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class Pet {
+    String url = "https://petstore.swagger.io/v2/pet";
+
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    @Test
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(url)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Snoopy" ))
+                .body("status", is("avaliable" ))
+        ;
+
+    }
+}
